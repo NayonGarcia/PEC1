0c8caef8a39bb99c69f99ae420dcd62f40f6925b
NayonGarcia <nayon.garcia.95@gmail.com>
2025-01-16 19:35:55 +0100

    carga asíncrona

--- src/app.js
+++ src/app.js
@@ -30,3 +30,17 @@ document.addEventListener("DOMContentLoaded", function () {
     // Mostrar la primera slide
     showSlide(currentIndex);
 });
+
+
+
+if ('loading' in HTMLImageElement.prototype) {
+    const images = document.querySelectorAll('img[loading="lazy"]');
+    images.forEach(img => {
+      img.src = img.dataset.src;
+    });
+  } else {
+    const script = document.createElement('script');
+    script.src =
+      'https://cdnjs.cloudflare.com/ajax/libs/lazysizes/5.1.2/lazysizes.min.js';
+    document.body.appendChild(script);
+  }
